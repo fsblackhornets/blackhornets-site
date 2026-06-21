2d80a28a3d90023b04fa8b71c7da744c8d12f338
Savke158 <dmi.157.22@student.pmf.uns.ac.rs>
2026-06-21 22:16:15 +0200

    fix: add map pin using q= format at center coordinates

--- frontend/pages/contact/helpers/map.js
+++ frontend/pages/contact/helpers/map.js
@@ -3,7 +3,7 @@ window.setupContactMap = () => {
 	if (!mapEl) return;
 
 	mapEl.innerHTML = `<iframe
-        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1221.2460984893037!2d19.851080151508224!3d45.24514145239378!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2srs!4v1782072795078!5m2!1sen!2srs"
+        src="https://www.google.com/maps?q=45.24514145239378,19.851080151508224&t=k&z=19&output=embed"
         width="600" height="450"
         style="border:0;width:100%;height:100%;"
         allowfullscreen loading="lazy"
