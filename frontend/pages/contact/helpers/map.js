9b557d1c6ca6f3425ad5194a545bba4b63b5c1d3
Savke158 <dmi.157.22@student.pmf.uns.ac.rs>
2026-06-21 22:30:53 +0200

    fix: move map pin north-northwest toward trees

--- frontend/pages/contact/helpers/map.js
+++ frontend/pages/contact/helpers/map.js
@@ -3,7 +3,7 @@ window.setupContactMap = () => {
 	if (!mapEl) return;
 
 	mapEl.innerHTML = `<iframe
-        src="https://www.google.com/maps?q=45.24514145239378,19.851080151508224&t=k&z=19&output=embed"
+        src="https://www.google.com/maps?q=45.24565,19.85045&t=k&z=19&output=embed"
         width="600" height="450"
         style="border:0;width:100%;height:100%;"
         allowfullscreen loading="lazy"
