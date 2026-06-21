4bf4fcf9531cfc464ddc7d76e1b05d926f47d7c2
Savke158 <dmi.157.22@student.pmf.uns.ac.rs>
2026-06-21 16:56:07 +0200

    debug: remove all who-we-are GSAP animations to isolate p3 visibility issue

--- frontend/pages/home/helpers/animations.js
+++ frontend/pages/home/helpers/animations.js
@@ -2,11 +2,4 @@ window.setupHomeAnimations = () => {
     if (typeof gsap === 'undefined') return;
 
     gsap.registerPlugin(ScrollTrigger);
-
-    gsap.fromTo('.who-we-are-content',
-        { y: 40, opacity: 0 },
-        { y: 0, opacity: 1, duration: 0.8, ease: 'power2.out',
-          immediateRender: false,
-          scrollTrigger: { trigger: '#who-we-are', start: 'top 80%' } }
-    );
 };
